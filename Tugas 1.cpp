26032314bca7113834acb7599fa2db93d7b0548f
jeffersonlie <81238664+jeffersonlie@users.noreply.github.com>
2022-01-18 20:18:40 +0700

    Add files via upload

---
+++ Tugas 1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a=0;
+int b=0;
+int x;
+while (b!=4) {
+    printf ("\nProgram Waterjug\n");
+    printf ("Air di A: %d\n", a);
+    printf ("Air di B: %d\n", b);
+    printf ("1. Isi A sampai penuh\n");
+    printf ("2. Isi B sampai penuh\n");
+    printf ("3. Kosongkan A\n");
+    printf ("4. Kosongkan B\n");
+    printf ("5. Pindahkan air dari A ke B sampai penuh\n");
+    printf ("6. Pindahkan air dari B ke A sampai penuh\n");
+    printf ("Pilih instruksi:");
+    cin >> x;
+
+    if (x==1) {
+        a=a+3;
+        if (a>3){a=3;}
+    }
+    if (x==2) {
+        b=b+5;
+        if (b>5){b=5;}
+    }
+    if (x==3) {
+        a=0;
+    }
+    if (x==4) {
+        b=0;
+    }
+    if (x==5) {
+        b=b+a;
+        a=0;
+        if (b>5) {
+            a=a+(b-5);
+            b=5;
+        }
+    }
+    if (x==6) {
+        a=a+b;
+        b=0;
+        if (a>3) {
+            b=b+(a-3);
+            a=3;
+        }
+    }
+
+}
+return 0;
+}
